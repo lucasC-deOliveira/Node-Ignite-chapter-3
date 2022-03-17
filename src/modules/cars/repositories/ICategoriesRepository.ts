c8aea219cbcd365c9285e2c70234a812eb1a2418
LucasC-deOliveira <luckericksen@gmail.com>
2022-03-17 14:35:13 -0300

    Alterando repositorio de categoria

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../model/Category";
+import { Category } from "../entities/Category";
 
 //  DTO => data transfer object = criar objeto para transerencia de dados entre uma classe e outra
 interface ICreateCategoryDTO{
@@ -8,9 +8,9 @@ interface ICreateCategoryDTO{
 
 
 interface ICategoriesRepository{
-findByName(name:string):Category;
-list(): Category[];
-create({name, description}:ICreateCategoryDTO):void;
+findByName(name:string):Promise<Category>;
+list(): Promise<Category[]>;
+create({name, description}:ICreateCategoryDTO):Promise<void>;
 }
 
 
--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,18 +1,20 @@
-import { Category } from "../../model/Category";
-import {ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository'
+import { getRepository, Repository } from "typeorm";
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository'
 
-class CategoriesRepository implements ICategoriesRepository{
-    private categories: Category[];
+class CategoriesRepository implements ICategoriesRepository {
+
+    private repository: Repository<Category>;
 
     private static INSTANCE: CategoriesRepository
 
     private constructor() {
-        this.categories = [];
+        this.repository = getRepository(Category);
     }
 
     //singleton
-    public static getInstance():CategoriesRepository {
-        if(!CategoriesRepository.INSTANCE){
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
             return CategoriesRepository.INSTANCE = new CategoriesRepository();
         }
         return CategoriesRepository.INSTANCE;
@@ -20,24 +22,25 @@ class CategoriesRepository implements ICategoriesRepository{
     }
 
 
-    create({name, description}:ICreateCategoryDTO):void {
-        const categorie = new Category()
-
-        Object.assign(categorie, { name, description, createdAt: new Date() })
-
-        this.categories.push(categorie)
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = this.repository.create({
+            name,
+            description
+        })
+        await this.repository.save(category)
     }
 
-    list():Category[] {
-        return this.categories
+    async list(): Promise<Category[]> {
+        const categories = await this.repository.find()
+        return categories;
     }
 
-    findByName(name:string):Category{
-        const category= this.categories.find(category =>  category.name === name)
+    async findByName(name: string): Promise<Category> {
+        const category = await this.repository.findOne({ name })
         return category;
     }
 
-   
+
 }
 
 
