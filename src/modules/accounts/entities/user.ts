6c83d2f7e949ab7848afab58debf2c8e63ca8b8c
LucasC-deOliveira <luckericksen@gmail.com>
2022-03-18 16:20:52 -0300

    Criando controller de users

--- src/modules/accounts/entities/user.ts
+++ src/modules/accounts/entities/user.ts
@@ -1,7 +1,7 @@
 import {v4 as uuid} from "uuid";
 import {Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm"
 
-@Entity()
+@Entity("users")
 class User{
     @PrimaryColumn()
     id:string;
